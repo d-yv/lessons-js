04367d4e5124960ed09f91c4f4b2278ca5aa95e2
d-yv <yuri.dubrovin@gmail.com>
2025-04-08 13:35:29 +0300

    add for

---
+++ js/for.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const library = {
+  books: 1923,
+  logBookCount() {
+    console.log(this.books);
+  },
+};
+
+function showBooks(callback) {
+  callback();
+}
+
+showBooks(library.logBookCount);
